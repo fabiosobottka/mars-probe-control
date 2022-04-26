2478e363151974de0f06f27314d95c2369e20255
DESKTOP-LKQ28G4\Fabio Sobottka <fabio.sarracini@gmail.com>
2022-04-26 01:35:58 -0300

    Incluindo usecase para explorar marte

---
+++ src/main/java/gov/nasa/mars/probe/control/usecases/ExploreMars.java
@@ -0,0 +1,15 @@
+package gov.nasa.mars.probe.control.usecases;
+
+import gov.nasa.mars.probe.control.entities.Plateau;
+import gov.nasa.mars.probe.control.entities.Probe;
+import gov.nasa.mars.probe.control.entities.ProbePosition;
+
+public class ExploreMars {
+	
+	public ProbePosition execute(final Probe probe, final Plateau plateau, final Character... commands) {
+		
+		
+		return null;
+	}
+
+}
