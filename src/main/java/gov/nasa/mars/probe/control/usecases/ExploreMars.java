faa9359032a526cb3eaa292dc1c49267af4c6dcb
DESKTOP-LKQ28G4\Fabio Sobottka <fabio.sarracini@gmail.com>
2022-04-26 03:35:08 -0300

    Implementancao do usecase para explorar marte

--- src/main/java/gov/nasa/mars/probe/control/usecases/ExploreMars.java
+++ src/main/java/gov/nasa/mars/probe/control/usecases/ExploreMars.java
@@ -1,15 +1,133 @@
 package gov.nasa.mars.probe.control.usecases;
 
+import org.springframework.stereotype.Service;
+
+import gov.nasa.mars.probe.control.entities.CardinalPoint;
 import gov.nasa.mars.probe.control.entities.Plateau;
 import gov.nasa.mars.probe.control.entities.Probe;
 import gov.nasa.mars.probe.control.entities.ProbePosition;
 
+@Service
 public class ExploreMars {
-	
-	public ProbePosition execute(final Probe probe, final Plateau plateau, final Character... commands) {
-		
-		
-		return null;
+
+	public ProbePosition execute(final Probe probe, final Plateau plateau, final String commands) {
+
+		validateProbeExploringInstructions(commands);
+
+		final char[] commandsArray = commands.toCharArray();
+		for (char direction : commandsArray) {
+
+			switch (direction) {
+			case 'L':
+				moveNinetyDegreesToLeft(probe);
+				break;
+			case 'R':
+				moveNinetyDegreesToRight(probe);
+				break;
+			case 'M':
+				moveForward(probe, plateau);
+				break;
+			default:
+				break;
+			}
+
+		}
+
+		return probe.getPosition();
+	}
+
+	//
+	// private
+	//
+
+	private void moveForward(final Probe probe, final Plateau plateau) {
+
+		final CardinalPoint cardinalPoint = probe.getPosition().getCardinalPoint();
+		final Integer probeUpperRightCoordinateY = plateau.getUpperRightCoordinateY().getValue();
+		final Integer probeUpperRightCoordinateX = plateau.getUpperRightCoordinateX().getValue();
+
+		Integer probeCoordinateY;
+		Integer probeCoordinateX;
+
+		switch (cardinalPoint) {
+		case NORTH:
+			probeCoordinateY = probe.getPosition().getCoordinateY().getValue();
+			if (probeCoordinateY < probeUpperRightCoordinateY) {
+				probe.getPosition().getCoordinateY().setValue(probeCoordinateY+1);
+			}
+			break;
+		case EAST:
+			probeCoordinateX = probe.getPosition().getCoordinateX().getValue();
+			if (probeCoordinateX < probeUpperRightCoordinateX) {
+				probe.getPosition().getCoordinateX().setValue(probeCoordinateX+1);
+			}
+			break;
+		case SOUTH:
+			probeCoordinateY = probe.getPosition().getCoordinateY().getValue();
+			if (probeCoordinateY > 0) {
+				probe.getPosition().getCoordinateY().setValue(probeCoordinateY-1);
+			}
+			break;
+		case WEST:
+			probeCoordinateX = probe.getPosition().getCoordinateX().getValue();
+			if (probeCoordinateX > 0) {
+				probe.getPosition().getCoordinateX().setValue(probeCoordinateX-1);
+			}
+			break;
+		default:
+			break;
+		}
+
+	}
+
+	private void moveNinetyDegreesToRight(final Probe probe) {
+
+		final CardinalPoint cardinalPoint = probe.getPosition().getCardinalPoint();
+
+		switch (cardinalPoint) {
+		case NORTH:
+			probe.getPosition().setCardinalPoint(CardinalPoint.EAST);
+			break;
+		case EAST:
+			probe.getPosition().setCardinalPoint(CardinalPoint.SOUTH);
+			break;
+		case SOUTH:
+			probe.getPosition().setCardinalPoint(CardinalPoint.WEST);
+			break;
+		case WEST:
+			probe.getPosition().setCardinalPoint(CardinalPoint.NORTH);
+			break;
+		default:
+			break;
+		}
+
+	}
+
+	private void moveNinetyDegreesToLeft(final Probe probe) {
+
+		final CardinalPoint cardinalPoint = probe.getPosition().getCardinalPoint();
+
+		switch (cardinalPoint) {
+		case NORTH:
+			probe.getPosition().setCardinalPoint(CardinalPoint.WEST);
+			break;
+		case WEST:
+			probe.getPosition().setCardinalPoint(CardinalPoint.SOUTH);
+			break;
+		case SOUTH:
+			probe.getPosition().setCardinalPoint(CardinalPoint.EAST);
+			break;
+		case EAST:
+			probe.getPosition().setCardinalPoint(CardinalPoint.NORTH);
+			break;
+		default:
+			break;
+		}
+
+	}
+
+	private void validateProbeExploringInstructions(final String commands) {
+
 	}
 
 }
