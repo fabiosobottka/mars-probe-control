ea9d73a6b8d53b8d2ade8e460d2c2d80795a2e99
DESKTOP-LKQ28G4\Fabio Sobottka <fabio.sarracini@gmail.com>
2022-04-27 06:08:58 -0300

    adicionando controller exception handler

---
+++ src/main/java/gov/nasa/mars/probe/control/exceptions/domain/StandardErrorResponse.java
@@ -0,0 +1,32 @@
+package gov.nasa.mars.probe.control.exceptions.domain;
+
+public class StandardErrorResponse {
+
+	private Integer status;
+	private String message;
+
+	public StandardErrorResponse() {}
+
+	public StandardErrorResponse(final Integer status, final String message) {
+		super();
+		this.status = status;
+		this.message = message;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(final Integer status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(final String message) {
+		this.message = message;
+	}
+
+}
---
+++ src/main/java/gov/nasa/mars/probe/control/exceptions/handler/ControllerExceptionHandler.java
@@ -0,0 +1,39 @@
+package gov.nasa.mars.probe.control.exceptions.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import gov.nasa.mars.probe.control.exceptions.InvalidProbeCoordinateException;
+import gov.nasa.mars.probe.control.exceptions.InvalidProbeInstructionsException;
+import gov.nasa.mars.probe.control.exceptions.domain.StandardErrorResponse;
+import gov.nasa.mars.probe.control.exceptions.domain.builder.StandardErrorResponseBuilder;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+	@ExceptionHandler(value = { InvalidProbeInstructionsException.class })
+	protected ResponseEntity<Object> handleConflict(final InvalidProbeInstructionsException exception, final WebRequest request) {
+
+		final StandardErrorResponse response = StandardErrorResponseBuilder.create()
+				.setStatus(400)
+				.setMessage(exception.getMessage())
+				.build();
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+	}
+	
+	@ExceptionHandler(value = { InvalidProbeCoordinateException.class })
+	protected ResponseEntity<Object> handleConflict(final InvalidProbeCoordinateException exception, final WebRequest request) {
+
+		final StandardErrorResponse response = StandardErrorResponseBuilder.create()
+				.setStatus(400)
+				.setMessage(exception.getMessage())
+				.build();
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+	}
+
+}
